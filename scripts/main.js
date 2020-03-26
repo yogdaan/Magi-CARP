1dd14895277d5949e698cb95e3ee0f2da3fabead
Xeon <harsh@anaek.com>
2020-03-26 06:45:01 +0530

    fix

--- scripts/main.js
+++ scripts/main.js
@@ -7,32 +7,34 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 $.getJSON("markers.json", function(json) {
     var markerdata = json; // this will show the info it in firebug console
+
+    function onEachFeature(feature, layer) {
+        console.log(feature)
+        layer.bindPopup(
+            "<div class='box'> <img src="+feature.properties.image+"> </div> Date: "+feature.properties.date+" <br>contributer: "+feature.properties.contributer+"<br>Community : "+feature.properties.community
+            );
+    }
+    
+    var jsonFeatures = [];
+    markerdata.forEach(function(point){
+        var lat = point.latitud;
+        var lon = point.longitud;
+        var feature = {type: 'Feature',
+            properties:point,
+    
+            geometry: {
+                type: 'Point',
+                coordinates: [lon,lat]
+            }
+        };
+        jsonFeatures.push(feature);
+    });
+    
+    var geoJson = { type: 'FeatureCollection', features: jsonFeatures };
+    
+    L.geoJSON(geoJson, {
+        onEachFeature: onEachFeature
+    }).addTo(mymap);
+
 });
 
-function onEachFeature(feature, layer) {
-    console.log(feature)
-    layer.bindPopup(
-        "<div class='box'> <img src="+feature.properties.image+"> </div> Date: "+feature.properties.date+" <br>contributer: "+feature.properties.contributer+"<br>Community : "+feature.properties.community
-        );
-}
-
-var jsonFeatures = [];
-markerdata.forEach(function(point){
-    var lat = point.latitud;
-    var lon = point.longitud;
-    var feature = {type: 'Feature',
-        properties:point,
-
-        geometry: {
-            type: 'Point',
-            coordinates: [lon,lat]
-        }
-    };
-    jsonFeatures.push(feature);
-});
-
-var geoJson = { type: 'FeatureCollection', features: jsonFeatures };
-
-L.geoJSON(geoJson, {
-    onEachFeature: onEachFeature
-}).addTo(mymap);
